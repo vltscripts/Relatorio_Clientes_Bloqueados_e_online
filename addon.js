b241b18c32b0c5077df16fcee30e4448140593a4
vltscripts <160883732+vltscripts@users.noreply.github.com>
2024-02-24 12:01:58 -0400

    Relatorio_Cli_Bloq_e_online

---
+++ addon.js
@@ -0,0 +1,8 @@
+/**
+* ABAIXO EXEMPLO DE COMO INCLUIR ITEM NO MENU PRINCIPAL DO SISTEMA DESCOMENTE AS LINHAS ABAIXO E LIMPE
+* SEU CACHE DO NAVEGADOR QUE NO MENU PROVEDOR SERA INCLUSO UM LINK PARA ADDON TESTE DE EXEMPLO
+*
+*/
+
+const minha_url = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '') + "/admin/";
+add_menu.conexoes('{"plink": "' + minha_url + 'addons/rel_bloq_ativos/", "ptext": "Relatorio Bloqueados e Online"}');
